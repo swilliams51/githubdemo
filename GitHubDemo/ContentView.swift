f465da28c66a489a289bcea2bf049feab0b7648e
steve williams <sjwilliams51@gmail.com>
2022-07-17 13:19:41 -0400

    I capitalized first world

--- GitHubDemo/ContentView.swift
+++ GitHubDemo/ContentView.swift
@@ -9,9 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Hello, World!")
+                .padding()
+            Text("Goodbye World")
+                .padding()
+                .font(.subheadline)
+        
+        }.background(Color.orange)
     }
+        
 }
 
 struct ContentView_Previews: PreviewProvider {
