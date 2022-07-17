d9a117c696cc675b5f0d770671a8ad2b35080563
steve williams <sjwilliams51@gmail.com>
2022-07-17 12:44:54 -0400

    Initial Commit

---
+++ GitHubDemo/GitHubDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubDemoApp.swift
+//  GitHubDemo
+//
+//  Created by Steven Williams on 7/17/22.
+//
+
+import SwiftUI
+
+@main
+struct GitHubDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
